189e7a23cc22467d8cd81118880a7d55758845ff
AndreasDEV <andreasmrdja16@gmail.com>
2019-07-03 14:01:09 +0200

    Setup express. Setup GET route and read JSON file. | Update #1 |

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 // Importing Dependencies
-const epxress = require('express');
+const epxress = require('express'),
+      fs = require('fs');
+
+
+const app = epxress();
+
+// * - Routes - * \\
+
+// Reading Tours JSON Data
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
+});
+
+
+
+
+
+
+
+
+// Listening Server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}..`);
+});
