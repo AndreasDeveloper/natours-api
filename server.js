f1463b4ecc7950376f9e187af5eda5348ccd4907
AndreasDEV <andreasmrdja16@gmail.com>
2019-07-04 02:12:24 +0200

    Setup ESLint airbnb and prettier. | Update #4 |

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // Setting Environmental Variables
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
+
+dotenv.config({ path: './config.env' });
 // Importing Modules
 const app = require('./app');
 
 // Listening Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}..`);
+  console.log(`App running on port ${port}..`);
 });
