01c68dacdfc00d2d75a2497456208e12d517730d
AndreasDEV <andreasmrdja16@gmail.com>
2019-07-04 19:18:56 +0200

    Setup MongoDB Database with Atlas. Setup mdb with env configurations. | Update #5 |

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 // Importing Modules
+const mongoose = require('mongoose');
 const app = require('./app');
+
+// Connecting MongoDB
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }).then(() => {
+  console.log('Database connected');
+});
 
 // Listening Server
 const port = process.env.PORT || 3000;
